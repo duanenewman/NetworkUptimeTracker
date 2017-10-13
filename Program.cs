21ad1f0a410b4e73871923aef553f67748d11229
Duane Newman <duane@alienarc.com>
2017-10-13 09:48:28 -0500

    Added IP Address tracking. Made pings run round robin through the list, more frequent connectivity checks, but less frequent repeat pings per minute each site. When a ping fails it will immediately ping all sites to determine if isolated to that host.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
@@ -9,64 +10,151 @@ using System.Threading.Tasks;
 
 namespace UptimeTracker
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var logFile = "uptimetracker.log";
-            var pingSites = new List<string>
-            {
-                "google.com",
-                "yahoo.com",
-                "comcast.com"
-            };
-            var interval = 30000;
+	public class HostInfo
+	{
+		public string Name { get; set; }
+		public IPAddress IPAddress { get; set; }
 
-            var lastRun = DateTime.MinValue;
-            var keepRunning = true;
+		public HostInfo(string name)
+		{
+			Name = name;
+			//UpdateIPAddress();
+		}
 
-            Task.WaitAll(
-                Task.Run(() => 
-                {
-                    Console.ReadKey();
-                    keepRunning = false;
-                }),
-                Task.Run(async () =>
-                {
-                    while (keepRunning)
-                    {
-                        if ((DateTime.Now - lastRun).TotalMilliseconds >= interval)
-                        {
-                            lastRun = DateTime.Now;
-                            using (var p = new Ping())
-                            {
-                                foreach (var s in pingSites)
-                                {
-                                    Console.Write($"Pinging {s}");
-                                    var status = "";
-                                    try
-                                    {
-                                        PingReply r = null;
-                                        r = p.Send(s, 1000);
-                                        status = IPStatus.Success.ToString();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        status = ex.Message;
-                                        var se = ex.InnerException as SocketException;
-                                        if (se != null)
-                                        {
-                                            status = se.SocketErrorCode.ToString();
-                                        }
-                                    }
-                                    Console.WriteLine($"\t{status}");
-                                    System.IO.File.AppendAllText(logFile, $"{DateTime.Now:s}\t{s}\t{status}\r\n");
-                                }
-                            }
-                        }
-                        await Task.Delay(100);
-                    }
-                }));
-        }
-    }
+		private void UpdateIPAddress()
+		{
+			IPAddress = Dns.GetHostAddresses(Name).First();
+		}
+	}
+
+	public class RoundRobinList<T> : List<T>
+	{
+		int lastEntryIndex = -1;
+
+		public T GetNext()
+		{
+			if (Count == 0) return default(T);
+
+			if (lastEntryIndex < Count) lastEntryIndex++;
+			if (lastEntryIndex >= Count) lastEntryIndex = 0;
+
+			return this[lastEntryIndex];
+		}
+
+		public IEnumerable<T> GetAllButLast()
+		{
+			if (Count == 0) return new T[0];
+
+			var lastItem = default(T);
+			if (lastEntryIndex >= 0 && lastEntryIndex < Count)
+			{
+				lastItem = this[lastEntryIndex];
+			}
+
+			return this.Where(i => !i.Equals(lastItem));
+		}
+	}
+
+	class Program
+	{
+
+		static string logFile = "uptimetracker.log";
+		static int interval = 10000;
+
+		static RoundRobinList<HostInfo> pingSites = new RoundRobinList<HostInfo>
+		{
+			new HostInfo("bing.com"),
+			new HostInfo("youtube.com"),
+			new HostInfo("github.com"),
+			new HostInfo("google.com"),
+			new HostInfo("yahoo.com"),
+			new HostInfo("comcast.com")
+		};
+
+		static DateTime lastRun = DateTime.MinValue;
+
+		static void Main(string[] args)
+		{
+			var keepRunning = true;
+
+			Task.WaitAll(
+				Task.Run(() =>
+				{
+					Console.ReadKey();
+					keepRunning = false;
+				}),
+				Task.Run(async () =>
+				{
+					while (keepRunning)
+					{
+						if ((DateTime.Now - lastRun).TotalMilliseconds >= interval)
+						{
+							var site = pingSites.GetNext();
+
+							if (!CheckSite(site))
+							{
+								foreach (var s in pingSites.GetAllButLast())
+								{
+									CheckSite(s);
+								}
+							}
+							lastRun = DateTime.Now;
+						}
+						await Task.Delay(100);
+					}
+				}));
+		}
+
+		private static bool CheckSite(HostInfo site)
+		{
+			var success = false;
+
+			Console.Write($"Pinging {site.Name} ({site.IPAddress?.ToString() ?? "?"})");
+			var status = "";
+			var logAddress = default(IPAddress);
+
+			try
+			{
+				using (var p = new Ping())
+				{
+					PingReply r = null;
+
+					if (site.IPAddress != null)
+					{
+						r = p.Send(site.IPAddress, 1000);
+					}
+					else
+					{
+						logAddress = site.IPAddress;
+						r = p.Send(site.Name, 1000);
+					}
+
+					status = r.Status.ToString();
+					success = r.Status == IPStatus.Success;
+					if (success && site.IPAddress == null)
+					{
+						logAddress = site.IPAddress = r.Address;
+					}
+					if (!success)
+					{
+						site.IPAddress = null;
+					} 
+				}
+			}
+			catch (Exception ex)
+			{
+				status = ex.Message;
+				var se = ex.InnerException as SocketException;
+				if (se != null)
+				{
+					status = se.SocketErrorCode.ToString();
+				}
+			}
+
+			Console.WriteLine($"\t{status}");
+			System.IO.File.AppendAllText(logFile, $"{DateTime.Now:s}\t{site.Name} ({logAddress})\t{status}\r\n");
+
+			return success;
+		}
+	}
 }
