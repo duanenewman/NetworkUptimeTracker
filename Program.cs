cde0932396d0c9f682359c1d518f2c1419237e31
Duane Newman <duane@alienarc.com>
2018-01-05 15:51:53 -0600

    Added support for command line arguments using Mono.Options, including the ability to set the log file, servers to ping, interval, and get help. Wrapped log file writing in a try/catch to it will continue to give console output with a bad logfile path (primarily for access denied, etc).

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -57,11 +59,13 @@ namespace UptimeTracker
 
 	class Program
 	{
-
-		static string logFile = "uptimetracker.log";
+		static string logFileName = "uptimetracker.log";
+		static string logFile = "";
 		static int interval = 10000;
 
-		static RoundRobinList<HostInfo> pingSites = new RoundRobinList<HostInfo>
+		static RoundRobinList<HostInfo> pingSites = new RoundRobinList<HostInfo>();
+
+		static readonly IReadOnlyList<HostInfo> defaultPingSites = new List<HostInfo>
 		{
 			new HostInfo("bing.com"),
 			new HostInfo("youtube.com"),
@@ -69,12 +73,65 @@ namespace UptimeTracker
 			new HostInfo("google.com"),
 			new HostInfo("yahoo.com"),
 			new HostInfo("comcast.com")
-		};
+		}.AsReadOnly();
 
 		static DateTime lastRun = DateTime.MinValue;
 
 		static void Main(string[] args)
 		{
+			var showHelp = false;
+
+			var options = new OptionSet { 
+				{ "l|logfile=", "the path of the logfile", l => {
+					if (!string.IsNullOrWhiteSpace(l))
+						logFile = l;
+					}
+				},
+				{ "s|server=", "a server to use in ping list (can be used more than once, if none are supplied it uses a default list).", s => {
+					if (!string.IsNullOrWhiteSpace(s))
+						pingSites.Add(new HostInfo(s));
+					}
+				},
+				{ "i|interval=", $"delay (in seconds) between ping attempts (default is {interval/1000} seconds)", i => {
+					if (!string.IsNullOrWhiteSpace(i))
+						interval = int.Parse(i) * 1000;
+					}
+				},
+				{ "?|h|help", "show this message and exit", h => showHelp = h != null },
+			};
+
+			List<string> extra;
+			try
+			{
+				// parse the command line
+				extra = options.Parse(args);
+			}
+			catch (OptionException e)
+			{
+				Console.Write("UptimeTracker: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `UptimeTracker --help' for more information.");
+				return;
+			}
+
+			if (showHelp)
+			{
+				Console.WriteLine("Usage: UptimeTracker.exe [OPTIONS]+");
+				Console.WriteLine("Periodically pings hosts in a round-robin fashion to track network connectivity.");
+				Console.WriteLine();
+				Console.WriteLine("Options:");
+				options.WriteOptionDescriptions(Console.Out);
+				return;
+			}
+
+			if (string.IsNullOrWhiteSpace(logFile))
+				logFile = Path.Combine(Path.GetTempPath(), logFileName);
+
+			if (pingSites.Count() == 0)
+			{
+				pingSites.AddRange(defaultPingSites);
+			}
+
 			var keepRunning = true;
 
 			Task.WaitAll(
@@ -151,9 +208,20 @@ namespace UptimeTracker
 			}
 
 			Console.WriteLine($" ({logAddress?.ToString() ?? "?"})\t{status}");
-			System.IO.File.AppendAllText(logFile, $"{DateTime.Now:s}\t{site.Name} ({logAddress})\t{status}\r\n");
+
+			try
+			{
+				File.AppendAllText(logFile, $"{DateTime.Now:s}\t{site.Name} ({logAddress})\t{status}\r\n");
+			}
+			catch (Exception ex)
+			{
+				//go on with life...
+				Console.WriteLine($"Error writing to log file: {ex.Message}");
+			}
 
 			return success;
 		}
+
+
 	}
 }
